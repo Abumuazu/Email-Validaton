a0af6bfe2d26abf72da1315284d42173b7eddb19
Decagon <e@Mzee.local>
2021-07-29 09:27:40 -0700

    week4

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -1,7 +1,14 @@
 import FileTree from './fileTree';
 
 export function createFileTree(input) {
+
   const fileTree = new FileTree();
+  let root = [input.shift()]
+
+  let sort = input.sort((a,b)=> a.id-b.id)
+ 
+  input = [...root, ...sort]
+ 
 
   for (const inputNode of input) {
     const parentNode = inputNode.parentId
